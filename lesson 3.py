5e78e033a9bd346e9b07f31308878be5aa753fe3
Grace-sims <gracesims2012@gmail.com>
2024-11-20 20:22:54 +0700

    Add files via upload

---
+++ lesson 3.py
@@ -0,0 +1,23 @@
+# 1 to 10 using the for loop
+for x in range (1,11,1):
+    print (x)
+
+print ()
+
+# 1 to 10 using the while loop
+y = 1
+while y <=10 :
+    print (y)
+    y = y + 1
+
+print ()
+
+for x in range (10,0,-1):
+    print (x)
+
+print ()
+
+y = 10
+while y >=1 :
+    print (y)
+    y = y - 1
